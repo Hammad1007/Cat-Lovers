bd5a55dad8104ab3c7c246952081969cadeac60d
Hammad1007 <hammadrashid2001@gmail.com>
2023-12-18 19:21:40 +0500

    Update the cat-lovers home page testing

--- cypress/e2e/homepage.spec.cy.js
+++ cypress/e2e/homepage.spec.cy.js
@@ -22,6 +22,22 @@ describe('Cat-Lovers Home Page', () => {
   })
 
   // it('CatLovers text can be hover over', () => {
-  //   cy.get('.navbar', 'CatLovers').should('be')
+  //   cy.get('.navbar-brand').should('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
+  //   .trigger('mouseover').should('have.css', 'color', 'rgb(0, 123, 255)')
   // })
+
+  it('CatLovers when clicked redirects me to the same page', () => {
+    cy.contains('.navbar-brand', 'CatLovers')
+    .should('be.visible')
+    .click()
+    cy.url().should('eq', 'http://localhost:3000/');
+  })
+
+  it('Home when clicked redirects me to the same page', () => {
+    cy.contains('.nav-item', 'Home')
+    .should('be.visible')
+    .click()
+    cy.url()
+    .should('eq', 'http://localhost:3000/');
+  })
 })
